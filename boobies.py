f5cc25d8c0812063cdb440ac689d26e0a5c3a41f
gaius_stonks <33606512+codingnerd267@users.noreply.github.com>
2020-11-09 10:19:34 -0800

    Update boobies.py

--- boobies.py
+++ boobies.py
@@ -3,5 +3,11 @@ print ("gaius be stonky")
 print ("zeke be monke")
 print ("monica is cool tho 8=============================D")
 for i in range(10):
-  print ("boobies")
+  boobieSize = i
+  if boobieSize<3:
+    print("beebs")
+  if boobieSize>2 and boobieSize<7:
+    print("bosoms")
+  if boobieSize>6 and boobieSize<11:
+    print("rack")
   
