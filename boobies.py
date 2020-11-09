d5ccac296ba3dac252c14d05744d52b22093a30e
gaius_stonks <33606512+codingnerd267@users.noreply.github.com>
2020-11-09 00:09:56 -0800

    Update boobies.py

--- boobies.py
+++ boobies.py
@@ -2,6 +2,6 @@ print("boobies are cool")
 print ("gaius be stonky")
 print ("zeke be monke")
 print ("monica is cool tho 8=============================D")
-for i in range(10) 
+for i in range(10):
   print ("boobies")
   
